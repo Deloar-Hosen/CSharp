a9489f8f558fef282a0750138d6ae7665f601dd3
Deloar <deloarhosen2003@gmail.com>
2024-10-13 15:37:26 +0600

    country name added

---
+++ country.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+
+public class CountryName
+{
+public static void Main()
+{
+Console.WriteLine($"My country name is America.");
+
+  
+    }
+
+  
+}
