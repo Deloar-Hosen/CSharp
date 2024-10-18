bc0e0ce01089d934c14f77341432a7f7517b2851
Deloar <deloarhosen2003@gmail.com>
2024-10-18 15:26:34 +0600

    Char data-type added

---
+++ Char.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Test
+{
+    public static void Main()
+    {
+// Declare a variable
+char ch = 'a';
+// Print the results on the console
+Console.WriteLine(
+"The code of '" + ch + "' is: " + (int)ch);
+ch = 'b';
+Console.WriteLine(
+"The code of '" + ch + "' is: " + (int)ch);
+ch = 'A';
+Console.WriteLine(
+"The code of '" + ch + "' is: " + (int)ch);
+        
+    }
+}
